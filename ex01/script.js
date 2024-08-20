2319fa1bd5b2f403fa890826c4a47643f1b2ded4
Adriellyc <165967593+Adriellyc@users.noreply.github.com>
2024-08-20 20:44:42 +0000

    .

--- ex01/script.js
+++ ex01/script.js
@@ -5,4 +5,4 @@ console.log("Mensagem impressa no console das ferramentas do desenvolvedor!");
 // console.log(confirm("esse aqui é um confirm"));
 // console.log(prompt("esse aqui é um prompt"));
 
-document.getElementById("titulo").innerHTML = "Alrerando o titulo 1";
+document.getElementById("titulo").innerHTML = "Alterando o titulo 1";
