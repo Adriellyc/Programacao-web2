b2fd9945b8f05eea3724af256d97f372ea3c1706
Adriellyc <165967593+Adriellyc@users.noreply.github.com>
2024-08-20 20:38:54 +0000

    .

--- ex01/script.js
+++ ex01/script.js
@@ -1,8 +1,8 @@
-alert("olá!");
-console.log("Mensagem impressa no console das ferramentas do desenvolvedor! ");
+// alert("Olá!");
+console.log("Mensagem impressa no console das ferramentas do desenvolvedor!");
 
-console.log(alert("esse aqui é um alert!"));
- console.log(confirm("esse aqui é um confirm"));
-console.log(prompt("esse aqui é um prompt"));
+// console.log(alert("esse aqui é um alert!"));
+// console.log(confirm("esse aqui é um confirm"));
+// console.log(prompt("esse aqui é um prompt"));
 
-document.getElementById("titulo").innerHTML = "Alterando o título 1";
+document.getElementById("titulo").innerHTML = "Alrerando o titulo 1";
