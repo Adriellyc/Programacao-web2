91e509e684f104eb6765cb4ba6bf53a82a71bc35
Adriellyc <165967593+Adriellyc@users.noreply.github.com>
2024-09-06 01:34:40 +0000

    .

--- Ex004/script.js
+++ Ex004/script.js
@@ -1,28 +1,29 @@
 function transformarEmJSON() {
-    const matricula = document.getElementById("matricula").Value;
-    const nome = document.getElementById("nome").Value;
-    const idade = document.getElementById("idade").Value;
-    const cpf = document.getElementById("cpf").Value;
+    
+    const matricula = document.getElementById('mat').value.trim();
+    const nome = document.getElementById('nome').value.trim();
+    const idade = document.getElementById('idade').value.trim();
+    const cpf = document.getElementById('cpf').value.trim();
 
- if (!matricula || !nome || !idade || !cpf) {
-alert("todos os campos devem ser preenchidos.");
-return;
-
-  }
-
-if (!Number.isInteger(Number(idade))){
- alert("A idade deve ser um número inteiro.");
- return;
- }
-const aluno = {
-    matricula: matricula,
-    nome: nome,
-    idade: parseInt(idade),
-    cpf : cpf 
-};
-
-document.getElementById("resultado").textContent = JSON.stringify(aluno,null,4);
-
-}
+    if (!matricula || !nome || !idade || !cpf) {
+        alert('Todos os campos devem ser preenchidos.');
+        return;
+    }
 
     
+    const idadeNumero = parseInt(idade, 10);
+    if (isNaN(idadeNumero) || idadeNumero <= 0) {
+        alert('A idade deve ser um valor inteiro positivo.');
+        return;
+    }
+
+    
+    const aluno = {
+        matricula,
+        nome,
+        idade: idadeNumero,
+        cpf
+    };
+
+    document.getElementById('jsonOutput').textContent = JSON.stringify(aluno, null, 4);
+}
